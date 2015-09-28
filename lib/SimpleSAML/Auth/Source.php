tractorcow/simplesamlphp

commit 28a732dc7560aa80dd1561218cb3eaa27605e6ee

Deprecate SimpleSAML_Default::initLogin() and move it to SimpleSAML_Auth_Source as a non-static method.

lib/SimpleSAML/Auth/Source.php
@@ -148,6 +148,56 @@ abstract class SimpleSAML_Auth_Source
 
 
     /**
+     * Start authentication.
+     *
+     * This method never returns.
+     *
+     * @param string|array $return The URL or function we should direct the user to after authentication. If using a
+     * URL obtained from user input, please make sure to check it by calling \SimpleSAML\Utils\HTTP::checkURLAllowed().
+     * @param string|null $errorURL The URL we should direct the user to after failed authentication. Can be null, in
+     * which case a standard error page will be shown. If using a URL obtained from user input, please make sure to
+     * check it by calling \SimpleSAML\Utils\HTTP::checkURLAllowed().
+     * @param array $params Extra information about the login. Different authentication requestors may provide different
+     * information. Optional, will default to an empty array.
+     */
+    public function initLogin($return, $errorURL = null, array $params = array())
+    {
+        assert('is_string($authId)');
+        assert('is_string($return) || is_array($return)');
+        assert('is_string($errorURL) || is_null($errorURL)');
+
+        $state = array_merge($params, array(
+            'SimpleSAML_Auth_Default.id' => $this->authId,
+            'SimpleSAML_Auth_Default.Return' => $return,
+            'SimpleSAML_Auth_Default.ErrorURL' => $errorURL,
+            'LoginCompletedHandler' => array(get_class(), 'loginCompleted'),
+            'LogoutCallback' => array(get_class(), 'logoutCallback'),
+            'LogoutCallbackState' => array(
+                'SimpleSAML_Auth_Default.logoutSource' => $this->authId,
+            ),
+        ));
+
+        if (is_string($return)) {
+            $state['SimpleSAML_Auth_Default.ReturnURL'] = $return;
+        }
+
+        if ($errorURL !== null) {
+            $state[SimpleSAML_Auth_State::EXCEPTION_HANDLER_URL] = $errorURL;
+        }
+
+        try {
+            $this->authenticate($state);
+        } catch (SimpleSAML_Error_Exception $e) {
+            SimpleSAML_Auth_State::throwException($state, $e);
+        } catch (Exception $e) {
+            $e = new SimpleSAML_Error_UnserializableException($e);
+            SimpleSAML_Auth_State::throwException($state, $e);
+        }
+        self::loginCompleted($state);
+    }
+
+
+    /**
      * Called when a login operation has finished.
      *
      * This method never returns.

lib/SimpleSAML/Auth/Default.php
@@ -15,63 +15,13 @@ class SimpleSAML_Auth_Default {
 
 
 	/**
-	 * Start authentication.
-	 *
-	 * This function never returns.
-	 *
-	 * @param string $authId  The identifier of the authentication source.
-	 * @param string|array $return The URL or function we should direct the
-	 * user to after authentication. If using a URL obtained from user input,
-	 * please make sure to check it by calling
-	 * \SimpleSAML\Utils\HTTP::checkURLAllowed().
-	 * @param string|NULL $errorURL The URL we should direct the user to after
-	 * failed authentication. Can be NULL, in which case a standard error page
-	 * will be shown. If using a URL obtained from user input, please make sure
-	 * to check it by calling \SimpleSAML\Utils\HTTP::checkURLAllowed().
-	 * @param array $params Extra information about the login. Different
-	 * authentication requestors may provide different information. Optional,
-	 * will default to an empty array.
+	 * @deprecated This method will be removed in SSP 2.0.
 	 */
 	public static function initLogin($authId, $return, $errorURL = NULL,
 		array $params = array()) {
 
-		assert('is_string($authId)');
-		assert('is_string($return) || is_array($return)');
-		assert('is_string($errorURL) || is_null($errorURL)');
-
-		$state = array_merge($params, array(
-			'SimpleSAML_Auth_Default.id' => $authId,
-			'SimpleSAML_Auth_Default.Return' => $return,
-			'SimpleSAML_Auth_Default.ErrorURL' => $errorURL,
-			'LoginCompletedHandler' => array(get_class(), 'loginCompleted'),
-			'LogoutCallback' => array(get_class(), 'logoutCallback'),
-			'LogoutCallbackState' => array(
-				'SimpleSAML_Auth_Default.logoutSource' => $authId,
-			),
-		));
-
-		if (is_string($return)) {
-			$state['SimpleSAML_Auth_Default.ReturnURL'] = $return;
-		}
-
-		if ($errorURL !== NULL) {
-			$state[SimpleSAML_Auth_State::EXCEPTION_HANDLER_URL] = $errorURL;
-		}
-
 		$as = SimpleSAML_Auth_Source::getById($authId);
-		if ($as === NULL) {
-			throw new Exception('Invalid authentication source: ' . $authId);
-		}
-
-		try {
-			$as->authenticate($state);
-		} catch (SimpleSAML_Error_Exception $e) {
-			SimpleSAML_Auth_State::throwException($state, $e);
-		} catch (Exception $e) {
-			$e = new SimpleSAML_Error_UnserializableException($e);
-			SimpleSAML_Auth_State::throwException($state, $e);
-		}
-		self::loginCompleted($state);
+		$as->initLogin($return, $errorURL, $params);
 	}
 
 

lib/SimpleSAML/Auth/Simple.php
@@ -133,7 +133,8 @@ class SimpleSAML_Auth_Simple {
 			$params[SimpleSAML_Auth_State::RESTART] = $restartURL;
 		}
 
-		SimpleSAML_Auth_Default::initLogin($this->authSource, $returnTo, $errorURL, $params);
+		$as = $this->getAuthSource();
+		$as->initLogin($this->authSource, $returnTo, $errorURL, $params);
 		assert('FALSE');
 	}
 

modules/oauth/www/registry.edit.php
@@ -17,7 +17,8 @@ if ($session->isValid($authsource)) {
 		throw new Exception('User ID is missing');
 	$userid = $attributes[$useridattr][0];
 } else {
-	SimpleSAML_Auth_Default::initLogin($authsource, \SimpleSAML\Utils\HTTP::getSelfURL());
+	$as = SimpleSAML_Auth_Source::getById($authsource);
+	$as->initLogin(\SimpleSAML\Utils\HTTP::getSelfURL());
 }
 
 function requireOwnership($entry, $userid) {

modules/oauth/www/registry.php
@@ -17,7 +17,8 @@ if ($session->isValid($authsource)) {
 		throw new Exception('User ID is missing');
 	$userid = $attributes[$useridattr][0];
 } else {
-	SimpleSAML_Auth_Default::initLogin($authsource, \SimpleSAML\Utils\HTTP::getSelfURL());
+	$as = SimpleSAML_Auth_Source::getById($authsource);
+	$as->initLogin(\SimpleSAML\Utils\HTTP::getSelfURL());
 }
 
 function requireOwnership($entry, $userid) {

commit e7d0e1975cf87261256ae74ea852552812b5aecb

MINOR: Fix incorrect assertion, broken in commit 28a732d.
Code was copied as-is from lib/SimpleSAML/Auth/Default.php, where the method
required $authId to be the first parameter, but now it's stored in self prior to
initLogin being called.

lib/SimpleSAML/Auth/Source.php
@@ -162,7 +162,7 @@ abstract class SimpleSAML_Auth_Source
      */
     public function initLogin($return, $errorURL = null, array $params = array())
     {
-        assert('is_string($authId)');
+        assert('is_string($this->authId)');
         assert('is_string($return) || is_array($return)');
         assert('is_string($errorURL) || is_null($errorURL)');
 