43a910d49106ea60dee9f771a6347649917098cf
madmatt <signups@madman.net.nz>
2015-10-02 14:03:31 +1300

    [NOT FOR MERGE]
    Update SimpleSAML\Utils\Config::getConfigDir() to look for _ss_environment.php file first

--- lib/SimpleSAML/Utils/Config.php
+++ lib/SimpleSAML/Utils/Config.php
@@ -66,21 +66,63 @@ class Config
      */
     public static function getConfigDir()
     {
-        $configDir    = dirname(dirname(dirname(__DIR__))) . '/config';
-        $configDirEnv = getenv('SIMPLESAMLPHP_CONFIG_DIR');
-        if ($configDirEnv !== false) {
-            if (!is_dir($configDirEnv)) {
-                throw new \InvalidArgumentException(
-                    sprintf(
-                        'Config directory specified by environment variable SIMPLESAMLPHP_CONFIG_DIR is not a ' .
-                        'directory.  Given: "%s"',
-                        $configDirEnv
-                    )
-                );
-            }
-            $configDir = $configDirEnv;
-        }
+        // Load _ss_environment.php. Code copied from silverstripe-framework/core/Constants.php
+		// Only load if the constant `SS_ENVIRONMENT_FILE` isn't already defined, as we can either run in or outside SS
 
-        return $configDir;
-    }
+		if(!defined('SS_ENVIRONMENT_FILE')) {
+			//define the name of the environment file
+			$envFile = '_ss_environment.php';
+			//define the dirs to start scanning from (have to add the trailing slash)
+			// we're going to check the realpath AND the path as the script sees it
+			$dirsToCheck = array(
+				realpath('.'),
+				dirname($_SERVER['SCRIPT_FILENAME'])
+			);
+			//if they are the same, remove one of them
+			if ($dirsToCheck[0] == $dirsToCheck[1]) {
+				unset($dirsToCheck[1]);
+			}
+			foreach ($dirsToCheck as $dir) {
+				//check this dir and every parent dir (until we hit the base of the drive)
+				// or until we hit a dir we can't read
+				while(true) {
+					//if it's readable, go ahead
+					if (@is_readable($dir)) {
+						//if the file exists, then we include it, set relevant vars and break out
+						if (file_exists($dir . DIRECTORY_SEPARATOR . $envFile)) {
+							define('SS_ENVIRONMENT_FILE', $dir . DIRECTORY_SEPARATOR . $envFile);
+							include_once(SS_ENVIRONMENT_FILE);
+							//break out of BOTH loops because we found the $envFile
+							break(2);
+						}
+					}
+					else {
+						//break out of the while loop, we can't read the dir
+						break;
+					}
+					if (dirname($dir) == $dir) {
+						// here we need to check that the path of the last dir and the next one are
+						// not the same, if they are, we have hit the root of the drive
+						break;
+					}
+					//go up a directory
+					$dir = dirname($dir);
+				}
+			}
+		}
+
+		$configDir = defined('REALME_CONFIG_DIR') ? constant('REALME_CONFIG_DIR') : null;
+
+		if(is_null($configDir) || !is_dir($configDir)) {
+			throw new \InvalidArgumentException(
+				sprintf(
+					'Config directory specified by _ss_environment variable REALME_CONFIG_DIR is not a ' .
+					'directory.  Given: "%s"',
+					$configDir
+				)
+			);
+		}
+
+		return $configDir;
+	}
 }
